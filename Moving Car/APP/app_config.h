f5ea580fc614747e5e8adf90775947d1a6640851
AbdelrhmanWalaa <abdelrhman-walaa96@hotmail.com>
2023-04-12 22:14:47 +0200

    update <app>: application skeleton

--- Moving Car/APP/app_config.h
+++ Moving Car/APP/app_config.h
@@ -3,13 +3,19 @@
  *
  *   Created on: Apr 10, 2023
  *       Author: Hacker Kermit - https://github.com/AbdelrhmanWalaa/Moving-Car-Project.git
- *  Description: 
+ *  Description: This file contains all Application (APP) pre-build configurations, through which user can configure before using the APP.
  */ 
-
 
 #ifndef APP_CONFIG_H_
 #define APP_CONFIG_H_
 
+/*******************************************************************************************************************************************************************/
+/* APP Configurations */
 
+
+
+/* End of Configurations */
+
+/*******************************************************************************************************************************************************************/
 
 #endif /* APP_CONFIG_H_ */
