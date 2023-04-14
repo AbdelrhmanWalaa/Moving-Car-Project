374e92f6ccb6320a1c24c31b1da84d93cb8bb0c3
Hossam Elwahsh <hacker2894@gmail.com>
2023-04-14 22:54:19 +0200

    refactor (dcm): naming conventions

--- Moving Car/HAL/dcm/dcm_program.c
+++ Moving Car/HAL/dcm/dcm_program.c
@@ -16,13 +16,13 @@
 /* Declaration and Initialization */
 
 
-ST_DCM_g_Config_t ST_g_carMotors[2] =
+ST_DCM_g_Config_t st_g_carMotors[2] =
 {
 	{ MOT0_EN_PIN_NUMBER_0, MOT0_EN_PIN_NUMBER_1, MOT0_PWM_PIN_NUMBER, MOT0_EN_PORT_NUMBER, MOT0_PWM_PORT_NUMBER },
 	{ MOT1_EN_PIN_NUMBER_0, MOT1_EN_PIN_NUMBER_1, MOT1_PWM_PIN_NUMBER, MOT1_EN_PORT_NUMBER, MOT1_PWM_PORT_NUMBER }
 };
 
-EN_DCM_FLAG DCM_g_stopFlag = FALSE;
+EN_DCM_FLAG en_g_stopFlag = FALSE;
 
 u8 * u8_g_shutdownFlag = NULL;
 
@@ -31,28 +31,28 @@ u8 * u8_g_shutdownFlag = NULL;
 EN_DCM_ERROR_T DCM_motorInit(u8 ** u8_a_shutdownFlag)
 {
     u8_g_shutdownFlag = *u8_a_shutdownFlag;
-	/*if (ST_g_carMotors == NULL)
+	/*if (st_g_carMotors == NULL)
 		return DCM_ERROR;
 	else
 	{*/
     u8 u8_a_loopCounter;
     for (u8_a_loopCounter = 0; u8_a_loopCounter < MOTORS_NUMBER; u8_a_loopCounter++)
     {
-        DIO_init(ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber0,
-            ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
+        DIO_init(st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber0,
+            st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
             DIO_OUT);
-        DIO_init(ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber1,
-            ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
+        DIO_init(st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber1,
+            st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
             DIO_OUT);
-        DIO_init(ST_g_carMotors[u8_a_loopCounter].DCM_g_motPWMPinNumber,
-            ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
+        DIO_init(st_g_carMotors[u8_a_loopCounter].DCM_g_motPWMPinNumber,
+            st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
             DIO_OUT);
 			
-		DIO_write(ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber0,
-			ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
+		DIO_write(st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber0,
+			st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
 			DIO_U8_PIN_HIGH);
-		DIO_write(ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber1,
-			ST_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
+		DIO_write(st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPinNumber1,
+			st_g_carMotors[u8_a_loopCounter].DCM_g_motEnPortNumber,
 			DIO_U8_PIN_LOW);	
 			
     }
@@ -63,18 +63,18 @@ EN_DCM_ERROR_T DCM_motorInit(u8 ** u8_a_shutdownFlag)
 
 
 /*******************************************************************************************************************************************************************/
-EN_DCM_ERROR_T DCM_changeDCMDirection(EN_DCM_MOTORSIDE DCM_a_motorNum)
+EN_DCM_ERROR_T DCM_changeDCMDirection(EN_DCM_MOTORSIDE en_a_motorNum)
 {
-	//if (DCM_a_motorNum > 2)
+	//if (en_a_motorNum > 2)
 		//return DCM_ERROR;
 	//else
 	//{
 
-		DIO_toggle(ST_g_carMotors[DCM_a_motorNum].DCM_g_motEnPinNumber0,
-			ST_g_carMotors[DCM_a_motorNum].DCM_g_motEnPortNumber
+		DIO_toggle(st_g_carMotors[en_a_motorNum].DCM_g_motEnPinNumber0,
+			st_g_carMotors[en_a_motorNum].DCM_g_motEnPortNumber
 		);
-		DIO_toggle(ST_g_carMotors[DCM_a_motorNum].DCM_g_motEnPinNumber1,
-			ST_g_carMotors[DCM_a_motorNum].DCM_g_motEnPortNumber
+		DIO_toggle(st_g_carMotors[en_a_motorNum].DCM_g_motEnPinNumber1,
+			st_g_carMotors[en_a_motorNum].DCM_g_motEnPortNumber
 		);
 	//}
 	return DCM_OK;
@@ -83,32 +83,32 @@ EN_DCM_ERROR_T DCM_changeDCMDirection(EN_DCM_MOTORSIDE DCM_a_motorNum)
 
 void DCM_stopDCM(void)
 {
-	DIO_write(ST_g_carMotors[0].DCM_g_motPWMPinNumber, ST_g_carMotors[0].DCM_g_motPWMPortNumber, DIO_U8_PIN_LOW);
-	DIO_write(ST_g_carMotors[1].DCM_g_motPWMPinNumber, ST_g_carMotors[1].DCM_g_motPWMPortNumber, DIO_U8_PIN_LOW);
-    DCM_g_stopFlag = FALSE;
+	DIO_write(st_g_carMotors[0].DCM_g_motPWMPinNumber, st_g_carMotors[0].DCM_g_motPWMPortNumber, DIO_U8_PIN_LOW);
+	DIO_write(st_g_carMotors[1].DCM_g_motPWMPinNumber, st_g_carMotors[1].DCM_g_motPWMPortNumber, DIO_U8_PIN_LOW);
+    en_g_stopFlag = FALSE;
 }
 
 /* ***********************************************************************************************/
 
-EN_DCM_ERROR_T DCM_setDutyCycleOfPWM(u8 DCM_a_dutyCycleValue)
+EN_DCM_ERROR_T DCM_setDutyCycleOfPWM(u8 u8_a_dutyCycleValue)
 {
 
-	if (DCM_a_dutyCycleValue > MAX_DUTY_CYCLE)
+	if (u8_a_dutyCycleValue > MAX_DUTY_CYCLE)
 		return DCM_ERROR;
 	else
 	{
-		u8 DCM_a_mappedDuty = DCM_a_dutyCycleValue / PERIOD_TIME;
+		u8 DCM_a_mappedDuty = u8_a_dutyCycleValue / PERIOD_TIME;
 		u16 u16_onTime = DCM_a_mappedDuty;
 		u16 u16_offTime = 10 - DCM_a_mappedDuty;
 
-		while (DCM_g_stopFlag != TRUE && (u8_g_shutdownFlag == NULL || *u8_g_shutdownFlag == 0))
+		while (en_g_stopFlag != TRUE && (u8_g_shutdownFlag == NULL || *u8_g_shutdownFlag == 0))
 		{
-			DIO_portWrite(ST_g_carMotors[0].DCM_g_motEnPortNumber, DIO_U8_PORT_HIGH, DIO_MASK_BITS_0_1);
+			DIO_portWrite(st_g_carMotors[0].DCM_g_motEnPortNumber, DIO_U8_PORT_HIGH, DIO_MASK_BITS_0_1);
 			TIMER_timer0Delay(u16_onTime);
-			DIO_portWrite(ST_g_carMotors[0].DCM_g_motEnPortNumber, DIO_U8_PORT_LOW, DIO_MASK_BITS_0_1);
+			DIO_portWrite(st_g_carMotors[0].DCM_g_motEnPortNumber, DIO_U8_PORT_LOW, DIO_MASK_BITS_0_1);
 			TIMER_timer0Delay(u16_offTime);
 		}
-		DCM_g_stopFlag = FALSE;
+		en_g_stopFlag = FALSE;
 	}
     return DCM_OK;
 }
@@ -116,7 +116,7 @@ EN_DCM_ERROR_T DCM_setDutyCycleOfPWM(u8 DCM_a_dutyCycleValue)
 /* ***********************************************************************************************/
 void DCM_updateStopFlag(void)
 {
-	DCM_g_stopFlag = TRUE;
+	en_g_stopFlag = TRUE;
 }
 /* ***********************************************************************************************/
 
